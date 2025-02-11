f70976852599c631c337fe7251b2e58b0ff23c0f
Ayush gairola <ayushgairola2002@gmail.com>
2025-02-11 14:06:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,6 @@ app.use(PaymentRouter);
 
 
 // listen the server at port 8080
-app.listen(8080,'localhost',()=>{
+app.listen(process.env.SERVER_PORT,'localhost',()=>{
 	console.log("server connected")
 })
