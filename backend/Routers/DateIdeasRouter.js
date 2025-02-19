bd99d5b9b6f47f5f1ad929c33f14950f04adcc5b
Ayush gairola <ayushgairola2002@gmail.com>
2025-02-19 19:06:30 +0530

    Delete backend directory

--- backend/Routers/DateIdeasRouter.js
+++
@@ -1,8 +0,0 @@
-const controller = require("../controller/VirtualCoachController");
-const express = require("express");
-const CoachPageRouter = express.router();
-
-
-
-CoachPageRouter.post("/session/coach/vitualdate",controller.data.Set_Meeting)
-.post("/find/resources/articles",controller.data.GetMaterial)
--- backend/backend/Routers/PaymentRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const PaymentRouter = express.Router();
-const {paypalWebhookHandler} = require("../controller/PaymentController");
-
-
-PaymentRouter.post("/payment",paypalWebhookHandler);
-
-
-module.exports = {PaymentRouter}
