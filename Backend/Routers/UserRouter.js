5da6364491f24515906ae384de55627896953452
ayush <ayushgairola2002@gmail.com>
2025-03-17 02:01:20 -0700

    testing server

--- Backend/Routers/UserRouter.js
+++ Backend/Routers/UserRouter.js
@@ -9,7 +9,24 @@ UserRouter.post("/Register",controller.data.upload.single("image"),controller.da
 .post("/login",controller.data.Login)
 .post("/resetPassword",verifyToken,controller.data.ResetPassword)
 .get("/welcome",controller.data.WelcomeMessage)
-.post('/verify',verifyToken)
+.post('/verify',(req,res)=>{
+	const token = req.cookies["auth-token"];
+
+	if(!token)return res.status(400).json({message:"No token found"});
+     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Token has expired" });
+      } else if (err.name === "JsonWebTokenError") {
+        return res.status(401).json({ message: "Invalid token" });
+      } else {
+        return res.status(500).json({ message: "Internal Server error" });
+      }
+    }else{
+    	return res.status(200).json({message:"Verified"});
+    }
+
+})
 .post("/media/upload/images",verifyToken,controller.data.upload.array("files"),controller.data.UploadMedia)
 .get("/profile/data",verifyToken,controller.data.SendUserData)
 .post("/profile/update",verifyToken,controller.data.UpdateProfile)
